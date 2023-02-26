a972a15d50c2630aff3cc35ae60262fc84ece550
Swarnim Paudyal <swarnimpaudyal55@gmail.com>
2023-02-26 13:42:41 +0545

    added meal components

--- README.md
+++ README.md
@@ -6,4 +6,5 @@ Building a Food Order App with ReactJs
 2. Created multiple components and assets sections.
 3. Added a Header Component with background Image in Layout.
 4. Created, Added and styled the "Cart" Button Component in Layout and Header Component.
-5. Added Meals Component
+5. Added Meals Components with dummy meals data and summarys.
+6. Added Individual Meal Items, created Card and meal components.
--- src/components/Meals/AvailbleMeals.js
+++ src/components/Meals/AvailbleMeals.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import Card from '../UI/Card';
 import styles from './AvailableMeals.module.css';
+import MealItem from './MealItem/MealItem';
 
 const DUMMY_MEALS = [
   {
@@ -30,11 +32,20 @@ const DUMMY_MEALS = [
 
 const AvailbleMeals = () => {
   const mealsList = DUMMY_MEALS.map((meal) => {
-    return <li>{meal.name}</li>;
+    return (
+      <MealItem
+        key={meal.id}
+        name={meal.name}
+        description={meal.description}
+        price={meal.price}
+      />
+    );
   });
   return (
     <section className={styles.meals}>
-      <ul>{mealsList}</ul>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
   );
 };
