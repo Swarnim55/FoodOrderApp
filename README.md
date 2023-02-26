9094b3946b53c05e758a62f0a9b173cf21e5f806
Swarnim Paudyal <swarnimpaudyal55@gmail.com>
2023-02-26 12:39:02 +0545

    Created Cart Button Component

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Building a Food Order App with ReactJs
 1. Created React App using create-react-app.
 2. Created multiple components and assets sections.
 3. Added a Header Component with background Image in Layout.
+4. Created, Added and styled the "Cart" Button Component in Layout and Header Component.
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import styles from './Header.module.css';
 import mealsImage from '../../assets/meals.jpg';
+import HeaderCartButton from './HeaderCartButton';
 
 const Header = (props) => {
   return (
     <>
       <header className={styles.header}>
         <h1>ReactMeals</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
       <div className={styles['main-image']}>
         <img src={mealsImage} alt="A table full of delicious food!" />
